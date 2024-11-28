0b1ecde0086f38d2cc67208ee74d7a822bc28ad9
Carlos Aguilar <caguilar36@gatech.edu>
2024-11-27 21:04:57 -0500

    Updating css and connecting with supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { createClient } from "@supabase/supabase-js";
+
+
+const supabase = createClient("https://ygsfkxzywnoexaqlwfgi.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlnc2ZreHp5d25vZXhhcWx3ZmdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI2NzUwNzYsImV4cCI6MjA0ODI1MTA3Nn0.oCUcKOwthuE5ygcnspO1KapA-i8aeqwFKZw_SIyzI7I");
 
 function App() {
+  const [regalos, setRegalos] = useState([]);
   const [count, setCount] = useState(0)
+
+  const onChange = (currentSlide) => {
+    console.log(currentSlide);
+  };
+
+  useEffect(() => {
+    getRegalos();
+  }, []);
+
+  async function getRegalos() {
+    const { data } = await supabase.from("Regalos").select();
+    setRegalos(data);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <header class="top-banner">
+        <div class="banner-content">
+        </div>
+      </header>
+      <h1>Lista de Regalo</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Seleccione el regalo que desea reservar para Alejandra e Isaías
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul id="gift-list">
+        {regalos.map((regalo) => (
+          <li key={regalo.id}>
+            <span class="gift-name">{regalo.nombre}</span>
+            <button class={"reserve-btn " + (regalo.reservado ? 'reserved' : '') }>
+              {regalo.reservado ? 'Reservado' : 'Reservar'}
+            </button>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
 
-export default App
+export default App;
